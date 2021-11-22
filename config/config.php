40eb95efa2f0f6bb879a9cf80c3d2132d0cc6c50
Senthur Raj <imsenthur@gmail.com>
2021-11-22 16:48:28 -0500

    Reverting interface to wlan0.

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ define('RASPI_BRAND_TEXT', 'Neurobionics');
 define('RASPI_CONFIG', '/etc/raspap');
 define('RASPI_CONFIG_NETWORK', RASPI_CONFIG.'/networking/defaults.json');
 define('RASPI_ADMIN_DETAILS', RASPI_CONFIG.'/raspap.auth');
-define('RASPI_WIFI_AP_INTERFACE', 'uap0');
+define('RASPI_WIFI_AP_INTERFACE', 'wlan0');
 define('RASPI_CACHE_PATH', sys_get_temp_dir() . '/raspap');
 
 // Constants for configuration file paths.
