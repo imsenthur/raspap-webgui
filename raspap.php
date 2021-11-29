2b7afca3261620ec4a3462675df809f20d440348
Senthur Raj <imsenthur@gmail.com>
2021-11-29 13:35:14 -0500

    Reverting to RaspAp default pwd.

--- raspap.php
+++ raspap.php
@@ -4,7 +4,7 @@ function getConfig()
 {
     $config = array(
         'admin_user' => 'admin',
-        'admin_pass' => 'neurobionics'
+        'admin_pass' => '$2y$10$YKIyWAmnQLtiJAy6QgHQ.eCpY4m.HCEbiHaTgN6.acNC6bDElzt.i'
     );
 
     if (file_exists(RASPI_CONFIG . '/raspap.auth')) {
